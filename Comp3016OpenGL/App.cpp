1b0d0b5fcac71d25b1aceb712077064beaf3d20a
WillSplaine <91668586+WillSplaine@users.noreply.github.com>
2023-12-09 17:44:09 +0000

    Commit added glm

--- Comp3016OpenGL/App.cpp
+++ Comp3016OpenGL/App.cpp
@@ -1,52 +1,135 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-int main(void)
-{
-    GLFWwindow* window;
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor; // New vertex attribute for color
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    out vec4 vertexColor; // New varying variable
 
-    
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    void main()
     {
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
+        vertexColor = vec4(aColor, 1.0); // Pass the color to the fragment shader
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    in vec4 vertexColor;
+    out vec4 FragColor;
+
+    void main()
+    {
+        FragColor = vertexColor;
+    }
+)";
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+float cameraSpeed = 2.5f;
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+glm::mat4 view;
+glm::mat4 projection;
+
+double lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLFWwindow* window;
+GLuint shaderProgram;
+
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void updateCameraVectors();
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "GLFW initialization failed\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 600, "OpenGL Cube and Camera Movement", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+        });
 
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error" << std::endl;
+    // Enable depth testing
+    glEnable(GL_DEPTH_TEST);
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "GLEW initialization failed\n";
+        glfwTerminate();
+        return -1;
     }
 
-    glfwTerminate();
-    return 0;
-}
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cerr << "Vertex shader compilation failed:\n" << infoLog << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cerr << "Fragment shader compilation failed:\n" << infoLog << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Shader program linking failed:\n" << infoLog << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+
+
+
+
